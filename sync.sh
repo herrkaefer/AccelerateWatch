560f2891e4e477c51899ce4393e7497ff25c5a16
herrkaefer <gloolar@gmail.com>
2016-06-20 22:50:49 +0800

    add sync.sh

---
+++ sync.sh
@@ -0,0 +1,3 @@
+cp -v AccelerateWatch/*.{h,c} DemoApp/AccelerateWatchDemo/Watch\ Extension/AccelerateWatch
+cp -R -v AccelerateWatch/kissfft/ DemoApp/AccelerateWatchDemo/Watch\ Extension/AccelerateWatch/kissfft
+cp -v DemoApp/AccelerateWatchDemo/Watch\ Extension/AccelerateWatch/*.swift AccelerateWatch/
